911aa877e7e83a917650c35189a35ff628eb007a
Harsh <harsh.singh0310@gmail.com>
2024-01-20 23:54:25 +0530

    data fetching of specific events

--- pages/events/[cat]/index.js
+++ pages/events/[cat]/index.js
@@ -1,15 +1,21 @@
 import Link from "next/link";
 
-const EventsPerCatPage=()=>{
-    return <div>
+const EventsPerCatPage=({data})=>{
+    return (<div>
         <h1>Events in London</h1>
+        <div>
+            {data.map(ev=>{
+                  <Link href='/event/event1'>Event 1</Link>
+            })}
         <Link href='/event/event1'>Event 1</Link>
         <Link href='/event/event2'>Event 2</Link>
         <Link href='/event/event3'>Event 3</Link>
         <Link href='/event/event4'>Event 4</Link>
         <Link href='/event/event5'>Event 5</Link>
         <Link href='/event/event6'>Event 6</Link>
+        </div>
     </div>
+    )
 }
 export default EventsPerCatPage;
 export async function getStaticPaths(){
