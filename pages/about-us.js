1c2836e8df64fb175650f9b72879f3bff4383c80
Harsh <harsh.singh0310@gmail.com>
2024-01-20 22:54:46 +0530

    changed layout

--- pages/about-us.js
+++ pages/about-us.js
@@ -3,7 +3,7 @@ const AboutUsPage=()=>{
         <div>
         <h1>About us Page</h1>
         <p>
-            {' '}
+          
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum, excepturi? Architecto tenetur, beatae consequuntur, alias minus enim laboriosam deleniti sed dolores possimus nam adipisci, veniam error magni eos blanditiis hic a eaque cum. Minus expedita earum dolore illum ea laudantium dolor adipisci nihil consectetur autem praesentium sequi corporis odit molestias id nobis maiores quos labore, suscipit ex. Accusantium, blanditiis quis.
         </p>
         </div>
